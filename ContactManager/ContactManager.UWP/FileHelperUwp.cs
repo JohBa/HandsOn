68cb5f6234504c2ae3ec9de3782ed4c11a6a1824
Johannes Bäurle <Johannes.Baeurle@aitgmbh.de>
2017-06-13 19:48:31 +0200

    fehlende filehelper implementierung

---
+++ ContactManager/ContactManager.UWP/FileHelperUwp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using ContactManager.Services;
+using ContactManager.UWP;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelperUwp))]
+namespace ContactManager.UWP
+{
+    public class FileHelperUwp : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}
---
+++ ContactManager/ContactManager.iOS/FileHelperIos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ContactManager.iOS;
+using ContactManager.Services;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(FileHelperIos))]
+namespace ContactManager.iOS
+{
+    public class FileHelperIos : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string docFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string libFolder = Path.Combine(docFolder, "..", "Library", "Databases");
+
+            if (!Directory.Exists(libFolder))
+            {
+                Directory.CreateDirectory(libFolder);
+            }
+
+            return Path.Combine(libFolder, filename);
+        }
+    }
+}
